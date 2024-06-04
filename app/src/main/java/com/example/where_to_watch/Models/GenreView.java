6c96e842f91edf5be86f242a10f8fa063cf880c8
lasuc <lucet.lucas1@gmail.com>
2024-06-04 09:24:48 +0200

    uhh

---
+++ app/src/main/java/com/example/where_to_watch/Models/GenreView.java
@@ -0,0 +1,12 @@
+package com.example.where_to_watch.Models;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.where_to_watch.R;
+
+public class GenreView extends AppCompatActivity  {
+
+    private RecyclerView recyclerView;
+
+}
